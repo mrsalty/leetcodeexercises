dfd64663a111c7a97172bf0340d7b8df5914f327
mrsalty <matteo.cunietti@gmail.com>
2018-11-25 09:15:02 +0000

    ProductExceptSelf

---
+++ src/LeetCodeExercises/ProductExceptSelf.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeExercises
+{
+    public class ProductExceptSelf
+    {
+        public class Solution
+        {
+            public int[] ProductExceptSelf(int[] nums)
+            {
+                int[] result = new int[nums.Length];
+                for (int i = 0, tmp = 1; i < nums.Length; i++)
+                {
+                    result[i] = tmp;
+                    tmp *= nums[i];
+                }
+                for (int i = nums.Length - 1, tmp = 1; i >= 0; i--)
+                {
+                    result[i] *= tmp;
+                    tmp *= nums[i];
+                }
+                return result;
+            }
+        }
+    }
+}
