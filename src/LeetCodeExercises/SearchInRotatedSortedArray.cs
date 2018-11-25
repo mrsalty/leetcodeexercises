c56ad6c5fdee27dce9410bc3bd82d9ae9f1b3e4b
mrsalty <matteo.cunietti@gmail.com>
2018-11-25 18:29:20 +0000

    SearchInRotatedSortedArray

---
+++ src/LeetCodeExercises/SearchInRotatedSortedArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LeetCodeExercises
+{
+    /// <summary>
+    /// https://leetcode.com/problems/search-in-rotated-sorted-array/
+    /// </summary>
+    public class SearchInRotatedSortedArray
+    {
+        public class Solution
+        {
+            public int Search(int[] nums, int target)
+            {
+                if (nums == null || nums.Length == 0) return -1;
+                return SearchRotatedSortedArray(nums, target);
+            }
+
+            private int SearchRotatedSortedArray(int[] nums, int target, bool trackIndex = false)
+            {
+                var middelItemIndex = nums.Length / 2;
+                var middleItem = nums[middelItemIndex];
+                var remainder = nums.Length % 2 > 0 ? 1 : 0;
+                var firstItem = nums[0];
+                var lastItem = nums[nums.Length - 1];
+
+                if (target == middleItem)
+                {
+                    return middelItemIndex;
+                }
+
+                //left side is ordered and target in range OR left side not ordered but target > firstitem
+                if (firstItem < middleItem && target <= middleItem && target >= firstItem ||
+                    firstItem > middleItem && target >= firstItem)
+                {
+                    var leftSide = new int[middelItemIndex];
+                    Array.Copy(nums, leftSide, middelItemIndex);
+                    if (trackIndex)
+                    {
+                        var result = SearchRotatedSortedArray(leftSide, target, true);
+                        if (result == -1) return -1;
+                        return middelItemIndex + result;
+                    }
+                    return SearchRotatedSortedArray(leftSide, target);
+                }
+
+                //right side is ordered and target in range OR right side not ordered but target < lastitem
+                if (lastItem > middleItem && target >= middleItem && target <= lastItem ||
+                    lastItem < middleItem && target <= lastItem)
+                {
+                    var rightSide = new int[nums.Length - 1 - middelItemIndex];
+                    Array.Copy(nums, middelItemIndex + 1, rightSide, 0, nums.Length - 1 - middelItemIndex);
+                    var result = SearchRotatedSortedArray(rightSide, target, true);
+                    if (result == -1) return -1;
+                    return middelItemIndex + result;
+                }
+
+                return -1;
+            }
+        }
+    }
+}
---
+++ tests/LeetCodeExercises.Tests.Unit/SearchInRotatedSortedArrayTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeExercises.Tests.Unit
+{
+    public class SearchInRotatedSortedArrayTests
+    {
+        private readonly SearchInRotatedSortedArray.Solution _sut;
+
+        public SearchInRotatedSortedArrayTests()
+        {
+            _sut = new SearchInRotatedSortedArray.Solution();
+        }
+
+        [TestCase(new int[] { }, 3, -1)]
+        [TestCase(new[] { 3 }, 3, 0)]
+        [TestCase(new[] { 25, 0 }, 0, 1)]
+        [TestCase(new[] { 25, 0 }, 25, 0)]
+        [TestCase(new[] { 1, 3, 5 }, 4, -1)]
+        [TestCase(new[] { 1, 3, 5 }, 1, 0)]
+        [TestCase(new[] { 5, 1, 3 }, 1, 1)]
+        [TestCase(new[] { 1, 3, 5 }, 5, 2)]
+        [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+        [TestCase(new[] { 25, 0, 1 }, 25, 0)]
+        [TestCase(new[] { 6, 7, 0, 1, 2, 4 }, 6, 0)]
+        [TestCase(new[] { 6, 7, 0, 1, 2, 4 }, 2, 4)]
+        [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+        [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 4, 0)]
+        [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 34, -1)]
+        public void WhenSortedRotatedArray_AndTarget_ThenReturnsIndex(int[] source, int target, int expected)
+        {
+            var result = _sut.Search(source, target);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
