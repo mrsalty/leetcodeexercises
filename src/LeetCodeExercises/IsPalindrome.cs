7f5cc673a05c67e3af8732824673f248f61cfacb
mrsalty <matteo.cunietti@gmail.com>
2018-11-28 07:03:14 +0000

    IsPalindrome

---
+++ src/LeetCodeExercises/IsPalindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCodeExercises
+{
+    /// <summary>
+    /// https://leetcode.com/problems/valid-palindrome/
+    /// </summary>
+    public class IsPalindrome
+    {
+        public class Solution
+        {
+            public bool IsPalindrome(string s)
+            {
+                int i = 0, j = s.Length - 1;
+                while (i < j)
+                {
+                    while (i < j && !Char.IsLetterOrDigit(s[i]))
+                    {
+                        i++;
+                    }
+
+                    while (i < j && !Char.IsLetterOrDigit(s[j]))
+                    {
+                        j--;
+                    }
+
+                    if (Char.ToLower(s[i]) != Char.ToLower(s[j]))
+                    {
+                        return false;
+                    }
+                    i++;
+                    j--;
+                }
+
+                return true;
+            }
+        }
+    }
+}
