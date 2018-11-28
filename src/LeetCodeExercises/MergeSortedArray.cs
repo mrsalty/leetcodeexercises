1d473ade1e81cac4ddeba3d4a701c8682fae0ae9
mrsalty <matteo.cunietti@gmail.com>
2018-11-28 07:33:51 +0000

    MergeSortedArray

---
+++ src/LeetCodeExercises/MergeSortedArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeExercises
+{
+    /// <summary>
+    /// https://leetcode.com/problems/merge-sorted-array/
+    /// </summary>
+    public class MergeSortedArray
+    {
+        public class Solution
+        {
+            public void Merge(int[] nums1, int m, int[] nums2, int n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    nums1[i + m] = nums2[i];
+                }
+
+                Array.Sort(nums1);
+            }
+        }
+    }
+}
---
+++ tests/LeetCodeExercises.Tests.Unit/MergeSortedArrayTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeExercises.Tests.Unit
+{
+    class MergeSortedArrayTests
+    {
+        private readonly MergeSortedArray.Solution _sut;
+
+        public MergeSortedArrayTests()
+        {
+            _sut = new MergeSortedArray.Solution();
+        }
+
+
+        [TestCase(new[] {1, 2, 3, 0, 0, 0}, 3, new[] {2, 5, 6}, 3, new[] {1, 2, 2, 3, 5, 6})]
+        public void GivenTwoSortedArrays_ThenTheyAreMergedCorrectly(int[] arr1, int m, int[] arr2, int n, int[] exptected)
+        {
+            _sut.Merge(arr1, m, arr2, n);
+            Assert.AreEqual(exptected, arr1);
+        }
+    }
+}
