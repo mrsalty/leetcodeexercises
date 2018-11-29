257209b6e531897f4df96fe5c075b56d843fb35d
mrsalty <matteo.cunietti@gmail.com>
2018-11-29 17:39:38 +0000

    WorkBreak (WIP)

---
+++ src/LeetCodeExercises/WordBreak.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace LeetCodeExercises
+{
+    /// <summary>
+    /// https://leetcode.com/problems/word-break/
+    /// </summary>
+    public class WordBreak
+    {
+        public class Solution
+        {
+            public bool WordBreak(string str, IList<string> wordDict)
+            {
+                return WordBreakRecurrent(str, str, wordDict);
+            }
+
+            public bool WordBreakRecurrent(string source, string str, IList<string> wordDict)
+            {
+                if (str == string.Empty) return true;
+
+                var isWordBreak = false;
+                for (var i = 0; i < wordDict.Count && !isWordBreak; i++)
+                {
+                    if (str.Contains(wordDict[i]))
+                    {
+                        var strWithoutCurrent = str;
+                        while (strWithoutCurrent.IndexOf(wordDict[i]) >= 0)
+                        {
+                            strWithoutCurrent = strWithoutCurrent.Remove(strWithoutCurrent.IndexOf(wordDict[i]),
+                                wordDict[i].Length);
+                        }
+
+                        var wordDictWithoutCurrent = new List<string>(wordDict);
+                        wordDictWithoutCurrent.RemoveAt(i);
+                        isWordBreak = WordBreak(strWithoutCurrent, wordDictWithoutCurrent);
+                    }
+                }
+
+                return isWordBreak;
+            }
+        }
+    }
+}
---
+++ tests/LeetCodeExercises.Tests.Unit/WordBreakTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCodeExercises.Tests.Unit
+{
+    public class WordBreakTests
+    {
+        private readonly WordBreak.Solution _sut;
+
+        public WordBreakTests()
+        {
+            _sut = new WordBreak.Solution();
+        }
+
+        [TestCase("leetcode", new[] { "leet", "code" }, true)]
+        [TestCase("applepenapple", new[] { "apple", "pen" }, true)]
+        [TestCase("catsandog", new[] { "cats", "dog", "sand", "and", "cat" }, false)]
+        [TestCase("cars", new[] { "car", "ca", "rs" }, true)]
+        [TestCase("cars", new[] { "car", "ca", "rs", "ars" }, true)]
+        [TestCase("cars", new[] { "car", "ca", "ars", "rs" }, true)]
+        [TestCase("applepie", new[] { "app", "le", "pi", "a", "e" }, true)]
+        [TestCase("ccbb", new[] { "bc", "cb" }, false)]
+        [TestCase("cbca", new[] { "bc", "ca" }, false)]
+        public void GivenStringAndArrayOfStrings_ThenCheckIfWordBreakApplies(string s, string[] words, bool exptected)
+        {
+            Assert.AreEqual(exptected, _sut.WordBreak(s, words.ToList()));
+        }
+    }
+}
