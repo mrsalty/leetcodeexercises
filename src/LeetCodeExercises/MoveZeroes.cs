5cbdbddf3eca313bb053fa366ac600afaed46afa
mrsalty <matteo.cunietti@gmail.com>
2018-12-02 11:13:15 +0000

    MoveZeroes

---
+++ src/LeetCodeExercises/MoveZeroes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeExercises
+{
+    public class MoveZeroes
+    {
+        public class Solution
+        {
+            public void MoveZeroes(int[] nums)
+            {
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    if (nums[i] == 0)
+                    {
+                        var indexToSwap = i + 1;
+                        while (indexToSwap < nums.Length - 1 && nums[indexToSwap] == 0)
+                        {
+                            indexToSwap++;
+                        }
+
+                        var tmp = nums[indexToSwap];
+                        nums[indexToSwap] = 0;
+                        nums[i] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
