5ced4a8580113852139b6bd0ba321998929229a8
mrsalty <matteo.cunietti@gmail.com>
2018-12-05 14:17:39 +0000

    RomanToInt

---
+++ tests/LeetCodeExercises.Tests.Unit/RomanToIntTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeExercises.Tests.Unit
+{
+    class RomanToIntTests
+    {
+        private readonly RomanToInteger.Solution _sut;
+
+        public RomanToIntTests()
+        {
+            _sut = new RomanToInteger.Solution();
+        }
+
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("IX", 9)]
+        [TestCase("XIX", 19)]
+        [TestCase("XVIII", 18)]
+        [TestCase("LVIII", 58)]
+        [TestCase("MCMXCIV", 1994)]
+        [TestCase("MCCXLVI", 1246)]
+        //1000, 100, 100, 10, 50, 5, 1
+        //----  --------  ------  ----
+        public void GivenRomanNumber_ThenTranslateToInteger(string roman, int expected)
+        {
+            Assert.AreEqual(expected, _sut.RomanToInt(roman));
+        }
+    }
+}
