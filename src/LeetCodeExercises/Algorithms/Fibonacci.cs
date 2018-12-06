e32c31746c5dce11e8c6762dcf52f9f72e61cfa8
mrsalty <matteo.cunietti@gmail.com>
2018-12-06 09:58:25 +0000

    Fibonacci via DP

---
+++ src/LeetCodeExercises/Algorithms/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeExercises.Algorithms
+{
+    /// <summary>
+    /// No LeetCode
+    /// </summary>
+    public class Fibonacci
+    {
+        public int GetFibonacciByIndex(int n)
+        {
+            var computed = new int?[n + 1];
+
+            return GetFibonacciDP(n, computed);
+        }
+
+        private int GetFibonacciDP(int n, int?[] computed)
+        {
+            if (computed[n] != null) return computed[n].Value;
+
+            if (n == 0 || n == 1) return 1;
+            var prev1 = GetFibonacciDP(n - 1, computed);
+            var prev2 = GetFibonacciDP(n - 2, computed);
+            computed[n] = prev1 + prev2;
+            return computed[n].Value;
+        }
+    }
+}
---
+++ tests/LeetCodeExercises.Tests.Unit/Algorithms/FibonacciTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeExercises.Algorithms;
+using NUnit.Framework;
+
+namespace LeetCodeExercises.Tests.Unit.Algorithms
+{
+    class FibonacciTests
+    {
+        private readonly Fibonacci _sut;
+
+        public FibonacciTests()
+        {
+            _sut = new Fibonacci();
+        }
+
+        //1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 5)]
+        [TestCase(5, 8)]
+        [TestCase(6, 13)]
+        [TestCase(15, 987)]
+        public void GivenIndex_ThenGetFibonacci(int n, int expected)
+        {
+            Assert.AreEqual(expected, _sut.GetFibonacciByIndex(n));
+        }
+    }
+}
