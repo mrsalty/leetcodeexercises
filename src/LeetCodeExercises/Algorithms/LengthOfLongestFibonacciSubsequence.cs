67a63274416f3c2182361bdb5951b41477ea12b8
mrsalty <matteo.cunietti@gmail.com>
2018-12-06 15:12:45 +0000

    LengthOfLongestFibonacciSubsequence (WIP)

---
+++ src/LeetCodeExercises/Algorithms/LengthOfLongestFibonacciSubsequence.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeExercises.Algorithms
+{
+    public class LengthOfLongestFibonacciSubsequence
+    {
+        public class Solution
+        {
+            public int LenLongestFibSubseq(int[] A)
+            {
+                int longest = 0;
+                int iterations = 0;
+                var fibItems = new Dictionary<int, List<Tuple<int, int>>>();
+                for (int i = A.Length - 1; i >= 2; i--)
+                {
+                    for (int j = i - 1; j > 0 && A[j] >= A[i] / 2; j--)
+                    {
+                        for (int k = 0; k < j; k++)
+                        {
+                            if (A[i] == A[j] + A[k])
+                            {
+                                iterations++;
+                                if (!fibItems.ContainsKey(A[i]))
+                                {
+                                    fibItems.Add(A[i], new List<Tuple<int, int>>
+                                    {
+                                        new Tuple<int, int>(A[j], A[k])
+                                    });
+                                }
+                                else
+                                {
+                                    fibItems[A[i]].Add(new Tuple<int, int>(A[j], A[k]));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < fibItems.Count; i++)
+                {
+                    var current = fibItems[i];
+                    while (current != null)
+                    {
+
+                    }
+                }
+
+                return longest;
+            }
+        }
+    }
+}
---
+++ tests/LeetCodeExercises.Tests.Unit/Algorithms/LengthOfLongestFibonacciSubsequenceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCodeExercises.Algorithms;
+using NUnit.Framework;
+
+namespace LeetCodeExercises.Tests.Unit.Algorithms
+{
+    public class LengthOfLongestFibonacciSubsequenceTests
+    {
+        private readonly LengthOfLongestFibonacciSubsequence.Solution _sut;
+
+        public LengthOfLongestFibonacciSubsequenceTests()
+        {
+            _sut = new LengthOfLongestFibonacciSubsequence.Solution();
+        }
+
+        [Ignore("WIP")]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 5)]     // [1,2,3,5,8]
+        [TestCase(new[] { 1, 3, 7, 11, 12, 14, 18 }, 3)]    // [1,11,12], [3,11,14] or [7,11,18]
+        public void GivenArrayOfNums_ThenCalculateLengthOfLongestFibonacciSubsequence(int[] nums, int expected)
+        {
+            Assert.AreEqual(expected, _sut.LenLongestFibSubseq(nums));
+        }
+    }
+}
